e76c56d8597ab5963f931091db973e757fab8043
gitlinksci <mrcmatuszak+gitlinks@gmail.com>
2017-06-12 12:13:01 +0200

    update travis

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo
+echo "Installing Gitlinks CLI, Hermes"
+
+INSTALL_FOLDER=~/.gitlinks
+
+current_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+if [ ! -d "$INSTALL_FOLDER" ]; then
+	mkdir $INSTALL_FOLDER
+fi
+
+cd "$INSTALL_FOLDER"
+curl -OsSf https://github.com/gitlinksci/spring-petclinic/raw/master/hermes-cl.tar.gz
+tar -xzf $INSTALL_FOLDER/hermes-cl.tar.gz
+cd $current_dir
+
+echo
+echo "run with $INSTALL_FOLDER/hermes-rust-ci"
+echo
